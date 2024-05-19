d5d7c2c472650f6bada3c5d7f8a3e8dd638c167d
zaid-sinpie <zaidmasuldar@gmail.com>
2024-05-19 12:10:33 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Header from "./components/Header";
+import Meals from "./components/Meals";
+import { CartContextProvider } from "./store/CartContext.jsx";
+import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
+import Cart from "./components/Cart.jsx";
+import Checkout from "./components/Checkout.jsx";
+
+function App() {
+  return (
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart/>
+        <Checkout/>
+      </CartContextProvider>
+    </UserProgressContextProvider>
+  );
+}
+
+export default App;
---
+++ src/components/Meals.jsx
@@ -0,0 +1,29 @@
+import MealItem from "./MealItem.jsx";
+import useHttp from "../hooks/useHttp.js";
+import Error from "./Error.jsx";
+
+const requestConfig = {};
+
+export default function Meals() {
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
+
+  if (isLoading) {
+    return <p className="center">Loading...</p>;
+  }
+
+  if(error){
+    return <Error title={"failed to fetch meal"} message={error}/>
+  }
+
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
+    </ul>
+  );
+}
